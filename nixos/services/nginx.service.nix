3651ddb6cbdfcd965bb72756f67ca3fb2952a3c0
Zhongheng Liu <z.liu@outlook.com.gr>
2024-08-09 16:15:27 +0800

    feat: new nginx configuration

--- nixos/services/nginx.service.nix
+++ nixos/services/nginx.service.nix
@@ -1,6 +1,21 @@
-{...}:
-{
-	services.nginx = {
-		enable = true;
-	};
+{pkgs, ...}: {
+  services.nginx = {
+    enable = true;
+    virtualHosts."localhost" = {
+      root = "/srv/public";
+      locations = {
+        "/" = {
+          root = "/srv/public";
+          extraConfig = ''
+            autoindex on;
+          '';
+        };
+      };
+      listenAddresses = [
+        "127.0.0.1"
+        "[::1]"
+      ];
+    };
+  };
+  networking.firewall.allowedTCPPorts = [80];
 }
