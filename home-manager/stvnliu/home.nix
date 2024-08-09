22b1d023c9d1e420993fa8280636b6be5968d80b
Zhongheng Liu <z.liu@outlook.com.gr>
2024-08-09 17:13:09 +0800

    feat: created signed commits

--- home-manager/stvnliu/home.nix
+++ home-manager/stvnliu/home.nix
@@ -54,13 +54,22 @@
 		protonvpn-gui
 			devenv
 	];
-	
-	programs.home-manager.enable = true;
 	programs.git = {
 		enable = true;
+		package = pkgs.gitFull;
 		userName = config.myDisplayName;
 		userEmail = config.myEmail;
+		extraConfig = {
+			push.autoSetupRemote = true;
+			commit.gpgsign = true;
+			gpg.format = "ssh";
+			gpg.ssh.allowedSignersFile = "/home/${config.myUserName}/.ssh/allowed_signers";
+			user.signingkey = "/home/${config.myUserName}/.ssh/id_ed25519.pub";
+			credential.helper = "libsecret";
+		};
 	};
+
+	programs.home-manager.enable = true;
 	programs.firefox.enable = true;
 # Nicely reload system units when changing configs
 	systemd.user.startServices = "sd-switch";
