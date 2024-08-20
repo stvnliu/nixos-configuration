b716af87bd189d199a77825241da744182577e1e
Zhongheng Liu <z.liu@outlook.com.gr>
2024-08-20 14:26:32 +0800

    fix: change dataDir and configDir
    This resolves the issue of not being able to sync the whole directory
    
    because of the existence of protected files.

--- nixos/services/syncthing.service.nix
+++ nixos/services/syncthing.service.nix
@@ -8,8 +8,8 @@
     syncthing = {
       enable = true;
       user = "${config.myUserName}";
-      dataDir = "/home/${config.myUserName}/Documents";
-      configDir = "/home/${config.myUserName}/Documents/.config/syncthing";
+      dataDir = "/home/${config.myUserName}/.config/syncthing";
+      configDir = "/home/${config.myUserName}/.config/syncthing";
     };
   };
   networking.firewall.allowedTCPPorts = [8384 22000];
