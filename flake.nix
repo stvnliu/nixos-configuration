4f6d4cba4a4ad259dc52de0ddc816f0906cb1719
Zhongheng Liu <z.liu@outlook.com.gr>
2024-09-26 16:49:28 +0300

    chore: bump flake.lock and refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -9,13 +9,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:danth/stylix";
+    stylix = {url = "github:danth/stylix";};
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
