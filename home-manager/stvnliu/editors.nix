e5f49cd89f06fb1ce359872f038a29eaf188c17d
Zhongheng Liu <z.liu@outlook.com.gr>
2024-10-06 10:59:46 +0300

    feat: add vscode

--- home-manager/stvnliu/editors.nix
+++ home-manager/stvnliu/editors.nix
@@ -11,4 +11,8 @@
   programs.nixvim = import ./nixvim;
   # dependencies for nixvim configuration
   home.packages = with pkgs; [ripgrep fd];
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode.fhs;
+  };
 }
