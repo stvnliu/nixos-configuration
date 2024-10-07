4060060af5e1cdcd7bc2c8b4886d9bc663a05eca
Zhongheng Liu <z.liu@outlook.com.gr>
2024-10-07 10:06:25 +0300

    feat: change git signingkey to GPG key

--- home-manager/stvnliu/programs/git.nix
+++ home-manager/stvnliu/programs/git.nix
@@ -12,9 +12,10 @@
     extraConfig = {
       push.autoSetupRemote = true;
       commit.gpgsign = true;
-      gpg.format = "ssh";
-      gpg.ssh.allowedSignersFile = "/home/${config.myUserName}/.ssh/allowed_signers";
-      user.signingkey = "/home/${config.myUserName}/.ssh/id_ed25519.pub";
+      #gpg.format = "ssh";
+      #gpg.ssh.allowedSignersFile = "/home/${config.myUserName}/.ssh/allowed_signers";
+      #user.signingkey = "/home/${config.myUserName}/.ssh/id_ed25519.pub";
+      user.signingkey = "DC8F48E7B4C40905";
       credential.helper = "libsecret";
     };
   };
