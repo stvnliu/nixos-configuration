712e45ca58174ad7b320bbaa12ec7a88ac4052d1
Zhongheng Liu <z.liu@outlook.com.gr>
2024-12-07 19:23:10 +0200

    fix: resolve problematic batmon alias

--- home-manager/stvnliu/shells/aliases/default.nix
+++ home-manager/stvnliu/shells/aliases/default.nix
@@ -2,12 +2,11 @@
   gil = "${pkgs.lazygit}/bin/lazygit";
   cd = "z";
   cdi = "zi";
-  ls = "${pkgs.lsd}/bin/lsd";
   cat = "${pkgs.bat}/bin/bat";
   ll = "ls -l";
   rm = "${pkgs.trash-cli}/bin/trash";
   osupdate = "${pkgs.nh}/bin/nh os switch";
   homeupdate = "${pkgs.nh}/bin/nh home switch";
-  batmon = "watch -n0 upower -i /org/freedesktop/UPower/devices/battery_BAT1";
+  #batmon = "watch -n0 upower -i /org/freedesktop/UPower/devices/battery_BAT1";
   inception = "${pkgs.pipx}/bin/pipx run copier copy --trust gh:DataChefHQ/Inception .";
 }
