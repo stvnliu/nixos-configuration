19330c1c8822e85efc6aef8511b5033470897f7b
Zhongheng Liu <z.liu@outlook.com.gr>
2025-01-04 20:12:37 +0200

    chore: deploy: change nix host to param

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 
 TMP_DIRNAME="/tmp/nix-config"
 GIT_REMOTE_SRC=$1
-NIX_HOST="<YOUR_HOSTNAME_HERE>"
+NIX_HOST=$2
 echo "Going to clone $GIT_REMOTE_SRC into $TMP_DIRNAME"
 mkdir "$TMP_DIRNAME"
 
@@ -12,7 +12,7 @@ git clone "$GIT_REMOTE_SRC" "$TMP_DIRNAME"
 prev=$PWD
 cd "$TMP_DIRNAME" || return
 echo "Now going to build nix configuration for $NIX_HOST to switch on next boot"
-sudo nixos-rebuild --flake .#$NIX_HOST boot
+sudo nixos-rebuild --flake .#"$NIX_HOST" boot
 
 cd "$prev" || return
 
