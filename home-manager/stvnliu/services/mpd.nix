20029c971a8029e2f055a6850b2e4b9143d33c09
Zhongheng Liu <z.liu@outlook.com.gr>
2025-05-29 19:07:54 +0300

    feat: integrate mpd-mpris
    allow mpd mpris

--- home-manager/stvnliu/services/mpd.nix
+++ home-manager/stvnliu/services/mpd.nix
@@ -1,16 +1,18 @@
 { pkgs, config, lib, ... }:
 lib.mkIf config.usingMusicPlayerDaemon {
   home.packages = [ pkgs.ncmpc ];
-  services.mpd = {
-    enable = true;
-    network.listenAddress = "any";
-    network.port = 6600;
-    extraConfig = ''
-      audio_output {
-        type "pipewire"
-        name "my pipewire output"
-      }
-    '';
+  services = {
+    mpd = {
+      enable = true;
+      network.listenAddress = "any";
+      network.port = 6600;
+      extraConfig = ''
+        audio_output {
+          type "pipewire"
+          name "my pipewire output"
+        }
+      '';
+    };
+    mpd-mpris = { enable = true; };
   };
-  services.mpd-mpris = { enable = true; };
 }
